f0b2d3434cb3a4763bda91495db60f68e9230def
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-03-06 18:13:54 +0100

    env

--- test/ws.test.ts
+++ test/ws.test.ts
@@ -1,104 +1,163 @@
+// deno-lint-ignore-file no-namespace require-await
+import { Future } from "npm:@hazae41/future@1.0.3";
 import { RpcCounter, RpcRequest, RpcRequestPreinit, RpcResponse, RpcResponseInit } from "npm:@hazae41/jsonrpc@1.0.5";
 import { NetworkMixin, base16_decode_mixed, base16_encode_lower, initBundledOnce } from "npm:@hazae41/network-bundle@1.2.1";
 
 await initBundledOnce()
 
-while (true) {
-  const session = crypto.randomUUID()
+export namespace WebSockets {
 
-  const socket = new WebSocket(`ws://localhost:8080/?session=${session}`)
+  export async function openOrThrow(url: string) {
+    const socket = new WebSocket(url)
+    const future = new Future<void>()
 
-  await new Promise((ok, err) => {
-    socket.addEventListener("open", ok)
-    socket.addEventListener("error", err)
-  })
-
-  const counter = new RpcCounter()
-  const events = new EventTarget()
-
-  const onRequest = (request: RpcRequest<unknown>, length: number) => {
-    events.dispatchEvent(new CustomEvent("request", { detail: [request, length] }))
-  }
-
-  const onResponse = (response: RpcResponse<unknown>, length: number) => {
-    events.dispatchEvent(new CustomEvent("response", { detail: [response, length] }))
-  }
-
-  const onMessage = (message: string) => {
-    const requestOrResponse = JSON.parse(message) as RpcRequest<unknown> | RpcResponse
-
-    if ("method" in requestOrResponse)
-      return onRequest(requestOrResponse, message.length)
-
-    return onResponse(requestOrResponse, message.length)
-  }
-
-  socket.addEventListener("message", (event) => {
-    if (typeof event.data !== "string")
-      return
-    return onMessage(event.data)
-  })
-
-  const requestOrThrow = async <T>(preinit: RpcRequestPreinit<unknown>, count = true) => {
-    const request = counter.prepare(preinit)
-    const message = JSON.stringify(request)
-
-    if (count) {
-      balanceBigInt -= BigInt(message.length)
-
-      while (balanceBigInt < 65536n)
-        await net_tip()
-
-      socket.send(message)
-    } else {
-      socket.send(message)
+    const onOpen = () => {
+      future.resolve()
     }
 
-    return await new Promise<RpcResponse<T>>(ok => {
-      const onResponse = async (event: Event) => {
-        const [response, length] = (event as CustomEvent<[RpcResponseInit<T>, number]>).detail
+    const onError = (event: Event) => {
+      future.reject(new Error("Errored", { cause: event }))
+    }
 
-        if (response.id !== request.id)
-          return
+    const onClose = (event: CloseEvent) => {
+      future.reject(new Error("Closed", { cause: event }))
+    }
 
-        if (count) {
-          balanceBigInt -= BigInt(length)
+    try {
+      socket.addEventListener("open", onOpen, { passive: true })
+      socket.addEventListener("error", onError, { passive: true })
+      socket.addEventListener("close", onClose, { passive: true })
 
-          while (balanceBigInt < 65536n)
-            await net_tip()
-          ok(RpcResponse.from(response))
-        } else {
-          ok(RpcResponse.from(response))
-        }
-      }
+      await future.promise
 
-      events.addEventListener("response", onResponse)
-    })
+      return socket
+    } finally {
+      socket.removeEventListener("open", onOpen)
+      socket.removeEventListener("error", onError)
+      socket.removeEventListener("close", onClose)
+    }
   }
 
-  let balanceBigInt = 0n
+}
 
-  const net_tip = async () => {
+export interface NetworkParams {
+  readonly chainIdString: string,
+  readonly contractZeroHex: string,
+  readonly receiverZeroHex: string,
+  readonly nonceZeroHex: string,
+  readonly minimumZeroHex: string
+}
+
+
+export class NetworkSession {
+
+  readonly uuid = crypto.randomUUID()
+
+  balance = 0n
+
+  constructor() { }
+
+}
+
+export class PricePerRequestNetworkSocket {
+
+  readonly events = new EventTarget()
+
+  #counter = new RpcCounter()
+
+  #onClean: () => void
+
+  constructor(
+    readonly session: NetworkSession,
+    readonly socket: WebSocket
+  ) {
+    const onMessage = (event: MessageEvent<unknown>) => {
+      this.#onMessage(event)
+    }
+
+    const onCloseOrError = () => {
+      this.#onClean()
+    }
+
+    socket.addEventListener("message", onMessage, { passive: true })
+    socket.addEventListener("close", onCloseOrError, { passive: true })
+    socket.addEventListener("error", onCloseOrError, { passive: true })
+
+    this.#onClean = () => {
+      socket.removeEventListener("message", onMessage)
+      socket.removeEventListener("close", onCloseOrError)
+      socket.removeEventListener("error", onCloseOrError)
+
+      this.#onClean = () => { }
+    }
+  }
+
+  [Symbol.dispose]() {
+    this.socket.close()
+  }
+
+  async requestOrThrow<T>(reqinit: RpcRequestPreinit<unknown>, price: bigint) {
+    const request = this.#counter.prepare(reqinit)
+    const message = JSON.stringify(request)
+
+    if (this.session.balance < price)
+      await this.#refill(price)
+
+    this.session.balance -= price
+
+    const future = new Future<RpcResponse<T>>()
+
+    const onResponse = (event: Event) => {
+      const response = (event as CustomEvent<RpcResponseInit<T>>).detail
+
+      if (response.id !== request.id)
+        return
+      future.resolve(RpcResponse.from(response))
+    }
+
+    const onError = (event: Event) => {
+      future.reject(new Error("Errored", { cause: event }))
+    }
+
+    const onClose = (event: CloseEvent) => {
+      future.reject(new Error("Closed", { cause: event }))
+    }
+
+    try {
+      this.events.addEventListener("response", onResponse, { passive: true })
+      this.socket.addEventListener("error", onError, { passive: true })
+      this.socket.addEventListener("close", onClose, { passive: true })
+
+      this.socket.send(message)
+
+      return await future.promise
+    } finally {
+      this.events.removeEventListener("response", onResponse)
+      this.socket.removeEventListener("error", onError)
+      this.socket.removeEventListener("close", onClose)
+    }
+  }
+
+  async #refill(price: bigint) {
+    await initBundledOnce()
+
     const {
       chainIdString,
       contractZeroHex,
       receiverZeroHex,
       nonceZeroHex,
       minimumZeroHex
-    } = await requestOrThrow<{
-      chainIdString: string,
-      contractZeroHex: string,
-      receiverZeroHex: string,
-      nonceZeroHex: string,
-      minimumZeroHex: string
-    }>({
+    } = await this.requestOrThrow<NetworkParams>({
       method: "net_get"
-    }, false).then(r => r.unwrap())
+    }, 0n).then(r => r.unwrap())
 
-    // const minimumBigInt = BigInt(minimumZeroHex)
+    const minimumBigInt = BigInt(minimumZeroHex)
 
-    // if (minimumBigInt > (2n ** 20n))
-    //   throw new Error("Minimum too high")
+    if (minimumBigInt > (2n ** 24n))
+      throw new Error("Minimum too high")
+
+    const minimumBase16 = minimumZeroHex.slice(2).padStart(64, "0")
+    const minimumMemory = base16_decode_mixed(minimumBase16)
 
     const chainIdBase16 = Number(chainIdString).toString(16).padStart(64, "0")
     const chainIdMemory = base16_decode_mixed(chainIdBase16)
@@ -112,19 +171,42 @@ while (true) {
     const nonceBase16 = nonceZeroHex.slice(2).padStart(64, "0")
     const nonceMemory = base16_decode_mixed(nonceBase16)
 
-    const mixinStruct = new NetworkMixin(chainIdMemory, contractMemory, receiverMemory, nonceMemory)
+    const mixin = new NetworkMixin(chainIdMemory, contractMemory, receiverMemory, nonceMemory)
 
-    const minimumBase16 = minimumZeroHex.slice(2).padStart(64, "0")
-    const minimumMemory = base16_decode_mixed(minimumBase16)
+    while (this.session.balance < price) {
+      const generatedStruct = mixin.generate(minimumMemory)
 
-    const generatedStruct = mixinStruct.generate(minimumMemory)
+      const secretMemory = generatedStruct.to_secret()
+      const secretBase16 = base16_encode_lower(secretMemory)
+      const secretZeroHex = `0x${secretBase16}`
 
-    const secretMemory = generatedStruct.to_secret()
-    const secretBase16 = base16_encode_lower(secretMemory)
-    const secretZeroHex = `0x${secretBase16}`
-
-    balanceBigInt += await requestOrThrow<string>({ method: "net_tip", params: [secretZeroHex] }, false).then(r => BigInt(r.unwrap()))
+      this.session.balance += await this.requestOrThrow<string>({ method: "net_tip", params: [secretZeroHex] }, 0n).then(r => BigInt(r.unwrap()))
+    }
   }
 
-  console.log(await requestOrThrow<unknown>({ method: "eth_blockNumber", params: [] }))
+  async #onMessage(event: MessageEvent<unknown>) {
+    if (typeof event.data !== "string")
+      return
+
+    const requestOrResponse = JSON.parse(event.data) as RpcRequest<unknown> | RpcResponse<unknown>
+
+    if ("method" in requestOrResponse)
+      return
+    const response = requestOrResponse
+
+    return void await this.#onResponse(response)
+  }
+
+  async #onResponse(response: RpcResponse<unknown>) {
+    this.events.dispatchEvent(new CustomEvent("response", { detail: response }))
+  }
+
 }
+
+const session = new NetworkSession()
+const socket = await WebSockets.openOrThrow(`ws://localhost:8080/?session=${session}`)
+const client = new PricePerRequestNetworkSocket(session, socket)
+
+console.log(await client.requestOrThrow<unknown>({ method: "net_search", params: [{}, { jsonRpcProtocols: ["eip155:1"] }] }, (2n ** 16n)))
+
+client.socket.close()
